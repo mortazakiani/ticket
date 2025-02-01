cb0ff0a4877e731d365a3b60436c3ee57db31f42
morteza <mortazakianitadi@gmail.com>
2025-02-01 23:43:55 +0330

    logg library

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"os"
+
+	"github.com/mortazakiani/ticket/internal/utiles"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
 func main() {
 	//load configs
+	config,err := utiles.LoadConfig(".")
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot load config,lunch app faild")
+	}
+
+	if config.APPDEBUG == "true" {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
 
 }
